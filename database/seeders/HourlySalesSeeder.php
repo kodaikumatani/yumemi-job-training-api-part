0ed85475e645f1ade0f6cd453190953b2cf8f411
kodaikumatani <koudai.aco5423@gmail.com>
2022-10-20 16:46:51 +0900

    fix:Changed from factory to sequential processing

--- database/seeders/HourlySalesSeeder.php
+++ database/seeders/HourlySalesSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\HourlySales;
+use App\Models\Product;
+use App\Models\Store;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +18,37 @@ class HourlySalesSeeder extends Seeder
      */
     public function run()
     {
-        HourlySales::factory()->count(5000)->save();
+        for ($i = -30; $i < 1; $i++) {
+            $qtyInfo = $this->initializeArray();
+            foreach (range(9, 20) as $hour) {
+                foreach (Store::pluck('id') as $store_id) {
+                    foreach (Product::pluck('id') as $product_id) {
+                        $qtyInfo[$store_id-1][$product_id-1] += rand(0,5);
+                        HourlySales::create([
+                            'date' => date('Y-m-d', strtotime((string)$i . ' day')),
+                            'hour' => $hour,
+                            'user_id' => User::orderBy('id', 'asc')->first()->value('id'),
+                            'store_id' => $store_id,
+                            'product_id' => $product_id,
+                            'quantity' => $qtyInfo[$store_id-1][$product_id-1],
+                        ]);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Store Quantity info.
+     *
+     * @return array
+     */
+    protected function initializeArray(): array
+    {
+        $data = array();
+        foreach (Store::pluck('id') as $store_id) {
+            $data[] = array_fill(0, Product::count(), 0);
+        }
+        return $data;
     }
 }
--- database/seeders/SalesSeeder.php
+++ database/seeders/SalesSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\Sales;
+use App\Models\Store;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +18,18 @@ class SalesSeeder extends Seeder
      */
     public function run()
     {
-        Sales::factory()->count(400)->create();
+        for ($i = -30; $i < 1; $i++) {
+            foreach (Store::pluck('id') as $store_id) {
+                foreach (Product::pluck('id') as $product_id) {
+                    Sales::create([
+                        'date' => date('Y-m-d', strtotime((string)$i . ' day')),
+                        'user_id' => User::orderBy('id', 'asc')->first()->value('id'),
+                        'store_id' => $store_id,
+                        'product_id' => $product_id,
+                        'quantity' => rand(1, 30),
+                    ]);
+                }
+            }
+        }
     }
 }
