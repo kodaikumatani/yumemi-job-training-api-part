9345dca361fe4517d2148a80a22bd44b0b92417f
kodaikumatani <koudai.aco5423@gmail.com>
2022-10-20 12:52:30 +0900

    fix:Move price attribute to product table

--- database/migrations/2022_10_06_053007_create_hourly_sales_table.php
+++ database/migrations/2022_10_06_053007_create_hourly_sales_table.php
@@ -26,8 +26,8 @@ return new class extends Migration
             $table->foreignId('product_id')
                 ->constrained('products')
                 ->onDelete('cascade');
-            $table->unsignedInteger('price');
             $table->unsignedInteger('quantity');
+            $table->unique(['date', 'hour', 'user_id', 'store_id', 'product_id']);
             $table->timestamps();
         });
     }
