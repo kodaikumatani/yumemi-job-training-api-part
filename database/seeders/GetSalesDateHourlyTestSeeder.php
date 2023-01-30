65db30e490603a6ec7713c5a923827205e303a1f
kodaikumatani <koudai.aco5423@gmail.com>
2023-01-30 17:06:09 +0900

    fix:Fix the data to be generated to one store

--- database/seeders/GetSalesDateHourlyTestSeeder.php
+++ database/seeders/GetSalesDateHourlyTestSeeder.php
@@ -19,18 +19,17 @@ class GetSalesDateHourlyTestSeeder extends Seeder
     public function run()
     {
         $user_id = User::orderBy('id', 'asc')->first()->value('id');
-        foreach (Store::orderBy('id', 'asc')->pluck('id') as $store_id ) {
-            foreach (Product::pluck('id') as $product_id) {
-                foreach (range(9, 20) as $idx => $hour ) {
-                    HourlySales::create([
-                        'date' => '2023-01-03',
-                        'hour' => $hour,
-                        'user_id' => $user_id,
-                        'store_id' => $store_id,
-                        'product_id' => $product_id,
-                        'quantity' => $idx,
-                    ]);
-                }
+        $store_id = Store::orderBy('id', 'asc')->first()->value('id');
+        foreach (Product::pluck('id') as $product_id) {
+            foreach (range(9, 20) as $idx => $hour) {
+                HourlySales::create([
+                    'date' => '2023-01-03',
+                    'hour' => $hour,
+                    'user_id' => $user_id,
+                    'store_id' => $store_id,
+                    'product_id' => $product_id,
+                    'quantity' => $idx,
+                ]);
             }
         }
     }
