5a7a34d8ae0bdbe7e3990218cc383f73a14662a5
kodaikumatani <koudai.aco5423@gmail.com>
2023-03-28 00:01:52 +0900

    feat:Add @vitejs/plugin-react

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
-            refresh: true,
-        }),
+        laravel(['resources/js/app.jsx']),
+        react(),
     ],
     build: {
         chunkSizeWarningLimit: 1000,
